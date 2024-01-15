97527a6c0b62666fb8590241f0e1d8443dc24cb7
senthil-k8s <senthilathiban0@gmail.com>
2024-01-15 16:25:22 +0530

    updated code

--- LeetCode/DailyStreaks/Find_Winner_and_Loser.java
+++ LeetCode/DailyStreaks/Find_Winner_and_Loser.java
@@ -1,19 +1,14 @@
 class Solution {
-    public List<List<Integer>> findWinners(int[][] matches) {
-        // TC -> O(n + nlogn), SC -> O(n)
-        HashMap<Integer, Integer> map = new HashMap<>();
-        List<List<Integer>> result = new ArrayList<>();
-
+    private static void findLoserCount(HashMap<Integer, Integer> map, int[][] matches){
         // find the loser count
-
         for(int it[] : matches){
             int u = it[0];
             int v = it[1];
             map.put(v, map.getOrDefault(v,0)+1);
         }
-        List<Integer> loser = new ArrayList<>();
-        List<Integer> winner = new ArrayList<>();
+    }
 
+    private static void findWinnerAndLoser(HashMap<Integer, Integer> map, int[][] matches, List<Integer> loser, List<Integer> winner){
         // logics to find winner and loser
         for(int it[] : matches){
             int u = it[0];
@@ -24,6 +19,15 @@ class Solution {
                 winner.add(u);
             }
         }
+    }
+    public List<List<Integer>> findWinners(int[][] matches) {
+        // TC -> O(n + nlogn), SC -> O(n)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> loser = new ArrayList<>();
+        List<Integer> winner = new ArrayList<>();
+        findLoserCount(map, matches)
+        findWinnerAndLoser(map, matches, loser, winner)
         result.add(winner);
         result.add(loser);
         Collections.sort(winner); // sort it
